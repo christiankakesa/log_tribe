9ad560fc21a3620e1aaf4c2394635937aceb2d1a
Christian Kakesa <christian.kakesa@gmail.com>
2016-01-03 23:30:07 +0100

    Fix typo.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,6 @@ require 'coveralls'
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([SimpleCov::Formatter::HTMLFormatter, Coveralls::SimpleCov::Formatter])
 SimpleCov.start
 
-lib = File.expand_path('../lib', __FILE__)
+lib = File.expand_path('../../lib', __FILE__)
 $LOAD_PATH.unshift lib unless $LOAD_PATH.include?(lib)
 require 'log_tribe'
